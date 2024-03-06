8fa9948b110c7347a6ece13fafd4fb569873c179
Aditya Shinde <adishinde611@gmail.com>
2024-03-06 21:00:52 +0530

    math function

---
+++ C/maths.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    printf("%f\n",ceil(3.6));
+    printf("%f\n",floor(4.5));
+    printf("%f\n",sqrt(16));
+    printf("%f\n",pow(2,2));
+    //printf("%d/n",abs(-4));
+    return 0;
+}
